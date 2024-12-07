216431bde61d197b5b6d45400272564d38d162d8
Sandip_Dutta_8 <ayandutta2005@gmail.com>
2024-12-07 21:35:20 +0530

    added write page frontend

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -7,7 +7,9 @@ import { signOut, useSession } from "next-auth/react";
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
 
-  const { status } = useSession();
+  // const { status } = useSession();
+
+  const status = 'authenticated'
 
   return (
     <>
